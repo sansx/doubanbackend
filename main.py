680e921e17ae9712f9f4c20a47cd7b8d6c198286
sansx <xiathxth@gmail.com>
2019-04-02 09:43:16 +0800

    use right sql

--- main.py
+++ main.py
@@ -2,21 +2,17 @@ import requests
 import json
 from connsql.connect import connectsql
 
-
 def getsqlconfig():
     with open(r"mysql.json", "r") as f:
         res = json.load(f)
     return res
 
-
 # connectsql(getsqlconfig())
-
 
 def getconfig():
     with open('config.json', "r") as f:
         res = json.load(f)
     return res
-
 
 def getMovieInfo(count):
     res = getconfig()
@@ -42,18 +38,22 @@ if res:
         # print(json.dumps(re["subjects"], ensure_ascii=False, indent=2))
         # cursor.execute('select * from movies')
         # values = cursor.fetchall()
-        # print(values) 
+        # print(values)
         with open("moviedata.json", "wt") as f:
-            f.write("aaa")
+            f.write(json.dumps(re["subjects"], ensure_ascii=False, indent=2))
             pass
         for info in re["subjects"]:
             print(info["title"], info["original_title"],
                   info["durations"], info["id"])
-            print("insert into movies (movie_id,original_title,title,durations) values ({}, '{}','{}','{}') where movie_id={} not in (select movie_id from movies)".format(info["id"], info["original_title"], info["title"], json.dumps(info["durations"]), info["id"]))
-            cursor.execute(
-                "insert into movies (movie_id,original_title,title,durations) values (%s, %s,%s,%s) where %s not in (select movie_id from movies)",
-                [info["id"], info["original_title"], info["title"], json.dumps(info["durations"]), info["id"]])
-            conn.commit()
+            # print("insert into movies (movie_id,original_title,title,durations) values ({}, '{}','{}','{}')".format(
+            #     info["id"], info["original_title"], info["title"], json.dumps(info["durations"]), info["id"]))
+            try:
+                cursor.execute(
+                    "insert into movies (movie_id,original_title,title,durations) values (%s, '%s','%s','%s')" % \
+                    (info["id"], info["original_title"], info["title"], json.dumps(info["durations"])))
+                conn.commit()
+            except:
+                conn.rollback()
         cursor.close()
         conn.close()
     except:
