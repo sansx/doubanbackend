1b203bb0235d0ea15a961dabe0cb1900659e7f48
sansx <xiathxth@gmail.com>
2019-04-10 22:44:05 +0800

    new template

--- microblog/app/routes.py
+++ microblog/app/routes.py
@@ -1,6 +1,8 @@
+from flask import render_template
 from app import app
 
 @app.route("/")
 @app.route("/index")
 def index():
-    return "Hello, World"
+    user={"username":"xiath"}
+    return render_template("index.html",title='home',user=user)
