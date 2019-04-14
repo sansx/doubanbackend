a5b7a1e855557813048d2914a5035cdda90d2b5b
sansx <xiathxth@gmail.com>
2019-04-14 22:04:55 +0800

    finish user login

--- microblog/app/models.py
+++ microblog/app/models.py
@@ -1,7 +1,7 @@
-from app import db
+from app import db, login
 from datetime import datetime
 from flask_login import UserMixin
-
+from werkzeug.security import generate_password_hash, check_password_hash
 
 class User(UserMixin,db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -13,6 +13,12 @@ class User(UserMixin,db.Model):
     def __repr__(self):
         return '<User {}>'.format(self.username)
 
+    def set_password(self,password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self,password):
+        return check_password_hash(self.password_hash, password)
+
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -22,3 +28,10 @@ class Post(db.Model):
 
     def __repr__(self):
         return '<Post {}>'.format(self.body)
+
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(id)
+
+
