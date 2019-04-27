9346dde0b6aea0596924ef6f696d2ff7b9fff571
sansx <xiathxth@gmail.com>
2019-04-27 23:52:56 +0800

    add main dir

--- microblog/app/__init__.py
+++ microblog/app/__init__.py
@@ -8,18 +8,16 @@ from flask_login import LoginManager
 from flask_mail import Mail
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
-from flask_babel import Babel, lazy_gettext as _l
 from config import Config
 
 db = SQLAlchemy()
 migrate = Migrate()
 login = LoginManager()
 login.login_view = 'auth.login'
-login.login_message = _l('Please log in to access this page.')
+login.login_message = 'Please log in to access this page.'
 mail = Mail()
 bootstrap = Bootstrap()
 moment = Moment()
-babel = Babel()
 
 
 def create_app(config_class=Config):
@@ -32,7 +30,6 @@ def create_app(config_class=Config):
     mail.init_app(app)
     bootstrap.init_app(app)
     moment.init_app(app)
-    babel.init_app(app)
 
     from app.errors import bp as errors_bp
     app.register_blueprint(errors_bp)
@@ -74,11 +71,6 @@ def create_app(config_class=Config):
         app.logger.info('Microblog startup')
 
     return app
-
-
-@babel.localeselector
-def get_locale():
-    return request.accept_languages.best_match(current_app.config['LANGUAGES'])
 
 
 from app import models
