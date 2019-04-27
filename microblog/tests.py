1296d08b973eb6052d2c540d89bfc168bf872727
sansx <xiathxth@gmail.com>
2019-04-27 23:21:21 +0800

    add create_app fn

--- microblog/tests.py
+++ microblog/tests.py
@@ -1,11 +1,12 @@
 from datetime import datetime, timedelta
 import unittest
-from app import app, db
+from app import app, db, current_app
 from app.models import User, Post
+
 
 class UserModelCase(unittest.TestCase):
     def setUp(self):
-        app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite://'
+        current_app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite://'
         db.create_all()
 
     def tearDown(self):
