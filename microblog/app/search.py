1ae6d3e7bf11fb794e4c0364aa12b4d97cd81889
sansx <xiathxth@gmail.com>
2019-05-01 23:55:04 +0800

    add search fn

---
+++ microblog/app/search.py
@@ -0,0 +1,44 @@
+from flask import current_app
+
+def add_to_index( index, model ):
+    if not current_app.elasticsearch:
+        return
+    payload = {}
+    for field in model.__searchable__:
+        payload[field] = getattr( model, field )
+    current_app.elasticsearch.index( index=index, doc_type=index, id=model.id, 
+                                     body=payload )
+
+
+def remove_from_index( index, model ):
+    if not current_app.elasticsearch:
+        return
+    current_app.elasticsearch.delete(index=index, doc_type=index, id=model.id)
+
+
+def query_index( index, query, page, per_page ):
+    if not current_app.elasticsearch:
+        return [], 0
+    # print(index, query, page, per_page)
+    search = current_app.elasticsearch.search(
+        index=index, doc_type=index,
+        body={'query': {'multi_match': {'query': query, 'fields': ['*']}},
+              'from': (page - 1) * per_page, 'size': per_page})
+    # print(search)
+    ids = [int(hit['_id']) for hit in search['hits']['hits']]
+    # print(ids)
+    # return ids, search['hits']['total']
+    return ids, search['hits']['total']['value']
+
+
+
+
+
+
+
+
+
+
+
+
+
