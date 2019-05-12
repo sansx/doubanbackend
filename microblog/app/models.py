ec516c21e07604d59872cbbd87d7f7341d6d7224
sansx <xiathxth@gmail.com>
2019-05-12 23:00:25 +0800

    add Task model

--- microblog/app/models.py
+++ microblog/app/models.py
@@ -1,5 +1,7 @@
 import jwt
 import json
+import redis
+import rq
 from datetime import datetime
 from hashlib import md5
 from time import time
@@ -66,6 +68,7 @@ class User(UserMixin, db.Model):
     posts = db.relationship('Post', backref='author', lazy='dynamic')
     about_me = db.Column(db.String(140))
     last_seen = db.Column(db.DateTime, default=datetime.utcnow)
+    tasks = db.relationship('Task', backref='user', lazy='dynamic')
     followed = db.relationship(
         'User', secondary=followers,
         primaryjoin=(followers.c.follower_id == id),
@@ -179,5 +182,21 @@ class Notification(db.Model):
         return json.loads(str(self.payload_json))
 
 
+class Task(db.Model):
+    id = db.Column(db.String(36), primary_key=True)
+    name = db.Column(db.String(128), index=True)
+    description = db.Column(db.String(128))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    complete = db.Column(db.Boolean, default=False)
 
+    def get_rq_job(self):
+        try:
+            rq_job = rq.job.Job.fetch(self.id, connection=current_app.redis)
+        except (redis.exceptions.RedisError, rq.exceptions.NoSuchJobError):
+            return None
+        return rq_job
+
+    def get_progress(self):
+        job = self.get_rq_job()
+        return job.meta.get('progress', 0) if job is not None else 100
 
