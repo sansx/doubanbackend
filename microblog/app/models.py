312c8e3469ce887ca57e08ea0d392cb5e8d9a04c
sansx <xiathxth@gmail.com>
2019-05-22 22:45:01 +0800

    token

--- microblog/app/models.py
+++ microblog/app/models.py
@@ -2,7 +2,9 @@ import jwt
 import json
 import redis
 import rq
-from datetime import datetime
+import base64
+import os
+from datetime import datetime, timedelta
 from hashlib import md5
 from time import time
 from flask import current_app, url_for
@@ -108,9 +110,30 @@ class User(PaginatedAPIMixin, UserMixin, db.Model):
     last_message_read_time = db.Column(db.DateTime)
     notifications = db.relationship('Notification', backref='user',
                                     lazy='dynamic')
+    token = db.Column(db.String(32), index=True, unique=True)
+    token_expiration = db.Column(db.DateTime)
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
+
+    def get_token(self, expires_in=3600):
+        now = datetime.utcnow()
+        if self.token and self.token_expiration > now + timedelta(seconds=60):
+            return self.token
+        self.token = base64.b64encode(os.urandom(24)).decode('utf-8')
+        self.token_expiration = now + timedelta(seconds=expires_in)
+        db.session.add(self)
+        return self.token
+
+    def revoke_token(self):
+        self.token_expiration = datetime.utcnow() - timedelta(seconds=1)
+
+    @staticmethod
+    def check_token(token):
+        user = User.query.filter_by(token=token).first()
+        if user is None or user.token_expiration < datetime.utcnow():
+            return None
+        return user
 
     def launch_task(self, name, description, *args, **kwargs):
         rq_job = current_app.task_queue.enqueue('app.tasks.' + name, self.id,
