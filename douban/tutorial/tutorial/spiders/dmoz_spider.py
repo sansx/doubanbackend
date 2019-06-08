2ff0667595b36aba72b6239ce66a39362e2f4ccd
sansx <xiathxth@gmail.com>
2019-06-08 22:40:11 +0800

    :clapper: new spider

--- douban/tutorial/tutorial/spiders/dmoz_spider.py
+++ douban/tutorial/tutorial/spiders/dmoz_spider.py
@@ -6,21 +6,21 @@ class DmozSpider(scrapy.Spider):
     name = "dmoz"
     allowed_domains = ["dmoztools.net"]
     start_urls = [
-        "http://www.dmoztools.net/Computers/Programming/Languages/Python/Books/",
-        "http://www.dmoztools.net/Computers/Programming/Languages/Python/Resources/"
+        "http://www.dmoztools.net/Computers/Programming/Languages/Python/",
     ]
 
     def parse(self, res):
-        filename = res.url.split("/")[-2] + '.html'
-        with open(filename, 'wb') as f:
-            f.write(res.body)
-        for sel in res.xpath('//ul/li'):
+        for href in res.css(".results.categories>.section-wrapper>.children>.cat-list>.cat-item>a::attr('href')"):
+            print(res.urljoin(href.extract()))
+            url = res.urljoin(href.extract())
+            yield scrapy.Request(url, callback=self.parse_dir_contents)
+
+
+    def parse_dir_contents(self, res):
+        for sel in res.xpath("//*[contains(@class, 'title-and-desc')]"):
             item = TutorialItem()
-            item["title"] = sel.xpath('a/text()').extract()
+            item["title"] = sel.xpath("a/*[contains(@class, 'site-title')]/text()").extract()
             item["link"] = sel.xpath('a/@href').extract()
-            item["desc"] = sel.xpath('text()').extract()
+            item["desc"] = sel.xpath("normalize-space(*[contains(@class, 'site-descr')]/text())").extract()
             yield item
-
-
-
 
