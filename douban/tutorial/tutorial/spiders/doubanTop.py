7acc782a9c5c169349b25490cb7b28926bcfd9db
sansx <xiathxth@gmail.com>
2019-06-09 23:25:43 +0800

    auto to next page

--- douban/tutorial/tutorial/spiders/doubanTop.py
+++ douban/tutorial/tutorial/spiders/doubanTop.py
@@ -2,6 +2,7 @@
 import scrapy
 import re
 
+
 class DoubantopSpider(scrapy.Spider):
     name = 'doubanTop'
     allowed_domains = ['movie.douban.com']
@@ -9,6 +10,8 @@ class DoubantopSpider(scrapy.Spider):
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36',
     }
+    count = 0
+
 
     def start_requests(self):
         yield scrapy.Request(self.start_urls[0], headers=self.headers)
@@ -16,6 +19,11 @@ class DoubantopSpider(scrapy.Spider):
     def parse(self, res):
         for href in res.css(".item>.info>.hd>a>span:first-child::text").extract():
             print("".join(href.split()))
+            self.count = self.count + 1
             # yield href
             pass
+        for href in res.css(".thispage").xpath("./following-sibling::*[1]/@href").extract():
+            if href :
+                yield scrapy.Request(res.urljoin(href), headers=self.headers)
+
         pass
