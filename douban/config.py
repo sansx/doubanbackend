32c7c63e199d4785a9b275b8fe72d9e5d1ed7929
sansx <xiathxth@gmail.com>
2019-06-13 09:54:58 +0800

    :bug: del config.py

--- douban/config.py
+++
@@ -1,7 +0,0 @@
-import os
-basedir = os.path.abspath(os.path.dirname(__file__))
-
-class Config(object):
-    # ...
-    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://xiath:a88869321@192.168.1.103/test"
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
