32648fe0c6eb820d6c2d4380493f43c02bbf236e
sansx <xiathxth@gmail.com>
2019-06-14 23:44:09 +0800

    full dbmovie models

--- douban/app/models.py
+++ douban/app/models.py
@@ -3,6 +3,8 @@ from app import db
 class dbmovie(db.Model):
     id = db.Column( db.Integer, primary_key=True )
     title = db.Column( db.Text )
+    rate = db.Column( db.Float(4,2) )
+    rateNum = db.Column( db.Integer )
 
     def __repr__(self):
         return '<dbmovie {}>'.format(self.title)    
